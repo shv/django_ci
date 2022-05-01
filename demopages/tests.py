44fc8bef1b705e52720f3f507e6acacd4014681c
Aleksey Sharypov <aleksey.sharypov@vkpay.com>
2022-05-01 13:31:44 +0300

    next step

--- demopages/tests.py
+++ demopages/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 
 # Create your tests here.
+class IndexTestCase(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+
+    def test_get_index_endpoint(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b'Hello, world')
